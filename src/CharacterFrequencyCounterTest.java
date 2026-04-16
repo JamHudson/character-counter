1cc9d3c906217120badbf4dc9f9388fb6edf60a3
JHud <219741840+JamHudson@users.noreply.github.com>
2026-04-16 11:13:10 -0700

    Tests + Bug-fixes, ironically untested

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,48 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
-    
+
+    // CharacterFrequencyCounter helloCounter;
+    // @BeforeTest
+    // void setup() {
+    //     helloCounter = new CharacterFrequencyCounter("hello");
+    // }
+
+    @Test
+    void testCharacterShowsUpMultipleTimesGiveCorrectFrequency() {
+        // Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+        // Act
+        int actual = counter.getFrequency('l');
+        // Assert
+        assertEquals(2,actual);
+    }
+
+    @Test
+    void testCharacterShowsUpOnce() {
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+
+        int actual = counter.getFrequency('h');
+
+        assertEquals(0,actual);
+    }
+
+    @Test
+    void testGetRelativeFrequencyReturns0ForMissingCharacter() {
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+
+        double actual = counter.getFrequency('x');
+
+        assertEquals(0.0,actual,0.0);
+    }
+
+    @Test
+    void testGetRelativeFrequencyReturns40() {
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+
+        double actual = counter.getFrequency('l');
+
+        assertEquals(40.0,actual,0.0001);
+    }
+
 }
